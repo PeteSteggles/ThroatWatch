0911fd4046dd569119f0fafa4486f8c5d78894c9
PeteSteggles <62215323+PeteSteggles@users.noreply.github.com>
2020-03-15 19:01:07 +0000

    init
    Basic toy camera application for modification

---
+++ app.js
@@ -0,0 +1,30 @@
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+window.addEventListener("load", cameraStart, false);
+
+
